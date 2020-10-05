3124335888a3e0227722eb7807ffc63de82f3538
Luna <54802902+QuantumXS202@users.noreply.github.com>
2020-10-05 14:44:29 +0200

    Add files via upload

---
+++ js-physics-engine-2020-master/html/16MyCard/script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+let image,numberOnARow,numberOnAColumn,sx,sy,sw,sh,x,y,w,h,index;
+
+image = new Image();
+image.src = "images/cardDeck.png";
+
+numberOnARow = 13;
+numberOnAColumn = 5;
+
+index = Math.floor(Math.random()*52);
+
+image.addEventListener('load',()=>{
+  sw = image.width/numberOnARow;
+  sh = image.height/numberOnAColumn;
+  animate();
+})
+
+function animate(){
+  sx = (index%numberOnARow)*sw;
+  sy = Math.floor(index/numberOnARow)*sh;
+  context.drawImage(image,sx,sy,sw,sh,100,100,sw,sh);
+}
